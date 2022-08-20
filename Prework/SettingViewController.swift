1ae3f729c3b528ef020486237f9ac6a5ab0efec3
vnesslange <langev@oregonstate.edu>
2022-08-19 23:29:47 -0400

    Update

---
+++ Prework/SettingViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingViewController.swift
+//  Prework
+//
+//  Created by Vanessa Lange on 8/17/22.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var tipPercent1: UITextField!
+    @IBOutlet weak var tipPercent2: UITextField!
+    @IBOutlet weak var tipPercent3: UITextField!
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func Set(_ sender: Any) {
+      
+        
+          }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
